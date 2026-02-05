520e7ca92b8dead211ddd0b7faea4eda583eab4b
Sai Khurana <44598300+saikhurana98@users.noreply.github.com>
2026-02-05 18:58:48 +0530

    cd: tribes add all origins (@saikhurana)

--- tribes-server/src/app.ts
+++ tribes-server/src/app.ts
@@ -33,7 +33,7 @@ const corsOptions = isDevMode
         "https://monkeytype.com",
         "https://dev.monkeytype.com",
         "https://monkeytype-test.rbh.makerspace.tools",
-        "monkeytype.rbh.makerspace.tools",
+        "https://monkeytype.rbh.makerspace.tools",
       ],
       methods: ["GET", "POST"],
       credentials: true,
